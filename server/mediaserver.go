04bccb73026c99445999062986b44b37e37caa67
0xb79orch <0xb79orch@gmail.com>
2022-07-28 06:48:47 -0500

    Update node status data to include addl info

--- server/mediaserver.go
+++ server/mediaserver.go
@@ -1578,6 +1578,7 @@ func (s *LivepeerServer) GetNodeStatus() *common.NodeStatus {
 		}
 	}
 	res := &common.NodeStatus{
+		NodeType:              s.LivepeerNode.NodeType.String(),
 		Manifests:             m,
 		InternalManifests:     make(map[string]string),
 		StreamInfo:            streamInfo,
@@ -1586,6 +1587,7 @@ func (s *LivepeerServer) GetNodeStatus() *common.NodeStatus {
 		GOArch:                runtime.GOARCH,
 		GOOS:                  runtime.GOOS,
 		OrchestratorPool:      []string{},
+		TranscoderSortMethod:  0,
 		RegisteredTranscoders: []common.RemoteTranscoderInfo{},
 		LocalTranscoding:      s.LivepeerNode.TranscoderManager == nil,
 		BroadcasterPrices:     make(map[string]*big.Rat),
@@ -1594,6 +1596,7 @@ func (s *LivepeerServer) GetNodeStatus() *common.NodeStatus {
 		res.InternalManifests[string(k)] = string(v)
 	}
 	if s.LivepeerNode.TranscoderManager != nil {
+		res.TranscoderSortMethod = s.LivepeerNode.TranscoderManager.TranscoderSortMethod()
 		res.RegisteredTranscodersNumber = s.LivepeerNode.TranscoderManager.RegisteredTranscodersCount()
 		res.RegisteredTranscoders = s.LivepeerNode.TranscoderManager.RegisteredTranscodersInfo()
 	}
