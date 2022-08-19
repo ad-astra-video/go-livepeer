594cd4d687acbec7158665a58d3b21d92d9501da
Ivan Poleshchuk <ivan.poleshchuk@outlook.com>
2022-08-19 12:05:31 +0500

    Session tear down logic for fast verification (#2381)

--- core/livepeernode.go
+++ core/livepeernode.go
@@ -88,8 +88,10 @@ type LivepeerNode struct {
 	// Thread safety for config fields
 	mu sync.RWMutex
 	// Transcoder private fields
-	serviceURI   url.URL
-	segmentMutex *sync.RWMutex
+	priceInfo     *big.Rat
+	serviceURI    url.URL
+	segmentMutex  *sync.RWMutex
+	StorageConfig *transcodeConfig
 }
 
 //NewLivepeerNode creates a new Livepeer Node. Eth can be nil.
