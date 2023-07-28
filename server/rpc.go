84c6a737d6470db7e5d2cf5f93f38a2e0cb40b3a
0xb79orch <0xb79orch@gmail.com>
2023-07-28 11:41:03 +0000

    update to transcoders having separate secrets

--- server/rpc.go
+++ server/rpc.go
@@ -46,12 +46,11 @@ var discoveryAuthWebhookCache = cache.New(authTokenValidPeriod, discoveryAuthWeb
 type Orchestrator interface {
 	ServiceURI() *url.URL
 	Address() ethcommon.Address
-	TranscoderSecret() string
 	Sign([]byte) ([]byte, error)
 	VerifySig(ethcommon.Address, string, []byte) bool
 	CheckCapacity(core.ManifestID) error
 	TranscodeSeg(context.Context, *core.SegTranscodingMetadata, *stream.HLSSegment) (*core.TranscodeResult, error)
-	ServeTranscoder(stream net.Transcoder_RegisterTranscoderServer, capacity int, capabilities *net.Capabilities)
+	ServeTranscoder(stream net.Transcoder_RegisterTranscoderServer, capacity int, capabilities *net.Capabilities, secret string)
 	TranscoderResults(job int64, res *core.RemoteTranscoderResult)
 	ProcessPayment(ctx context.Context, payment net.Payment, manifestID core.ManifestID) error
 	TicketParams(sender ethcommon.Address, priceInfo *net.PriceInfo) (*net.TicketParams, error)
