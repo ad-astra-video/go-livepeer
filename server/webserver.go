65e0206834f7e5f197080c9ce1bd3dafc55f39b9
0xb79orch <0xb79orch@gmail.com>
2023-07-28 11:43:36 +0000

    add manual orch selection with http push

--- server/webserver.go
+++ server/webserver.go
@@ -49,6 +49,7 @@ func (s *LivepeerServer) cliWebServerHandlers(bindAddr string) *http.ServeMux {
 	mux.Handle("/setBroadcastConfig", mustHaveFormParams(setBroadcastConfigHandler()))
 	mux.Handle("/getBroadcastConfig", getBroadcastConfigHandler())
 	mux.Handle("/getAvailableTranscodingOptions", getAvailableTranscodingOptionsHandler())
+	mux.Handle("/getSessionPoolInfo", s.getSessionPoolInfoHandler())
 
 	// Rounds
 	mux.Handle("/currentRound", currentRoundHandler(client))
