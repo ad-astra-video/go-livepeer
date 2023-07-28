cca4ecf4102877e489976062b64609e54f19d19d
0xb79orch <0xb79orch@gmail.com>
2023-07-28 11:41:03 +0000

    update to transcoders having separate secrets

--- cmd/livepeer/starter/starter.go
+++ cmd/livepeer/starter/starter.go
@@ -478,6 +478,16 @@ func StartLivepeer(ctx context.Context, cfg LivepeerConfig) {
 		if !*cfg.Transcoder {
 			n.TranscoderManager = core.NewRemoteTranscoderManager()
 			n.Transcoder = n.TranscoderManager
+			//set up transcoder secrets
+			t_err := n.GetTranscoderSecrets()
+			if t_err == nil {
+				for k, v := range n.TranscoderManager.TranscoderSecrets() {
+					glog.Infof("TranscoderSecret loaded:  %s  active: %t", k, v)
+				}
+			} else {
+				glog.Errorf("Error getting TranscoderSecrets: %s", t_err)
+			}
+
 		}
 	} else if *cfg.Transcoder {
 		n.NodeType = core.TranscoderNode
@@ -1101,7 +1111,7 @@ func StartLivepeer(ctx context.Context, cfg LivepeerConfig) {
 		// if http addr is not provided, listen to all ifaces
 		// take the port to listen to from the service URI
 		*cfg.HttpAddr = defaultAddr(*cfg.HttpAddr, "", n.GetServiceURI().Port())
-		if !*cfg.Transcoder && n.OrchSecret == "" {
+		if !*cfg.Transcoder && len(n.OrchSecret) == 0 {
 			glog.Fatal("Running an orchestrator requires an -orchSecret for standalone mode or -transcoder for orchestrator+transcoder mode")
 		}
 	}
@@ -1190,7 +1200,7 @@ func StartLivepeer(ctx context.Context, cfg LivepeerConfig) {
 	}()
 
 	if n.NodeType == core.TranscoderNode {
-		if n.OrchSecret == "" {
+		if len(n.OrchSecret) == 0 {
 			glog.Fatal("Missing -orchSecret")
 		}
 		if len(orchURLs) <= 0 {
--- core/livepeernode.go
+++ core/livepeernode.go
@@ -208,3 +208,27 @@ func (n *LivepeerNode) SetMaxSessions(s int) {
 		lpmon.MaxSessions(MaxSessions)
 	}
 }
+
+func (n* LivepeerNode) GetTranscoderSecrets() error {
+	n.TranscoderManager.RTmutex.Lock()
+	defer n.TranscoderManager.RTmutex.Unlock()
+	secrets, err := n.Database.GetTranscoderSecrets()
+	//include OrchSecret
+	n.TranscoderManager.transcoderSecrets[n.OrchSecret] = true
+	//get other transcoder secrets
+	if err == nil {
+		for k, v := range secrets {
+			n.TranscoderManager.transcoderSecrets[k] = v
+		}
+		return nil
+	} else {
+		return err
+	}
+}
+
+func (n* LivepeerNode) UpdateTranscoderSecret(secret string, active bool) {
+	n.TranscoderManager.RTmutex.Lock()
+	defer n.TranscoderManager.RTmutex.Unlock()
+	n.Database.UpdateTranscoderSecret(secret, active)
+	n.TranscoderManager.transcoderSecrets[secret] = active
+}
