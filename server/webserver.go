a7f6391b0c3ccb260e2397b57d5cf137e99bb7b4
0xb79orch <0xb79orch@gmail.com>
2023-08-24 02:39:15 +0000

    add webserver handler to get orchestrator info

--- server/webserver.go
+++ server/webserver.go
@@ -50,6 +50,7 @@ func (s *LivepeerServer) cliWebServerHandlers(bindAddr string) *http.ServeMux {
 	mux.Handle("/getBroadcastConfig", getBroadcastConfigHandler())
 	mux.Handle("/getAvailableTranscodingOptions", getAvailableTranscodingOptionsHandler())
 	mux.Handle("/getSessionPoolInfo", s.getSessionPoolInfoHandler())
+	mux.Handle("/getOrchestratorInfo", s.getOrchestratorInfoHandler())
 
 	// Rounds
 	mux.Handle("/currentRound", currentRoundHandler(client))
