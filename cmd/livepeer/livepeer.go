8ef713d21ccc10a1be54afa9b46045855c0746e8
0xb79 <0xb79orch@gmail.com>
2024-06-27 20:17:30 -0500

    change aiWorkerNoManagedContainers to aiWorkerUseManagedContainers

--- cmd/livepeer/livepeer.go
+++ cmd/livepeer/livepeer.go
@@ -155,7 +155,7 @@ func parseLivepeerConfig() starter.LivepeerConfig {
 	cfg.AIModels = flag.String("aiModels", *cfg.AIModels, "Set models (pipeline:model_id) for AI worker to load upon initialization")
 	cfg.AIModelsDir = flag.String("aiModelsDir", *cfg.AIModelsDir, "Set directory where AI model weights are stored")
 	cfg.AIRunnerImage = flag.String("aiRunnerImage", *cfg.AIRunnerImage, "Set the docker image for the AI runner: Example - livepeer/ai-runner:0.0.1")
-	cfg.AIWorkerNoManagedContainers = flag.Bool("aiWorkerNoManagedContainers", *cfg.AIWorkerNoManagedContainers, "set to true if want to no use managed containers with AI worker")
+	cfg.AIWorkerUseManagedContainers = flag.Bool("aiWorkerUseManagedContainers", *cfg.AIWorkerUseManagedContainers, "set to true if want to use managed containers with AI worker")
 	cfg.PricePerCapability = flag.String("pricePerCapability", *cfg.PricePerCapability, `json list of prices per capability/model or path to json config file. Use "model_id": "default" to price all models in a pipeline the same. Example: {[{"gateway": "default", "pipeline": "text-to-image", "model_id": "stabilityai/sd-turbo", "priceperunit": 1000, "pixelsperunit": 1}, {"gateway": "0x0", "pipeline": "upscale", "model_id": "default", priceperunit": 1200, "pixelsperunit": 1}]}`)
 
 	// Onchain:
