43949765eac13f56830b1d9e563e01e138653b6f
Brad P <brad@ad-astra.video>
2024-08-21 16:46:56 -0500

    enable selecting orchestrator to get AI job

--- server/ai_mediaserver.go
+++ server/ai_mediaserver.go
@@ -97,10 +97,12 @@ func handle[I, O any](ls *LivepeerServer, decoderFunc func(*I, *http.Request) er
 		requestID := string(core.RandomManifestID())
 		ctx = clog.AddVal(ctx, "request_id", requestID)
 
+		orchAddr := r.Header.Get("OrchAddr")
 		params := aiRequestParams{
 			node:        ls.LivepeerNode,
 			os:          drivers.NodeStorage.NewSession(requestID),
 			sessManager: ls.AISessionManager,
+			orchAddr:    orchAddr,
 		}
 
 		var req I
@@ -158,10 +160,12 @@ func (ls *LivepeerServer) ImageToVideo() http.Handler {
 
 		clog.V(common.VERBOSE).Infof(ctx, "Received ImageToVideo request imageSize=%v model_id=%v async=%v", req.Image.FileSize(), *req.ModelId, async)
 
+		orchAddr := r.Header.Get("OrchAddr")
 		params := aiRequestParams{
 			node:        ls.LivepeerNode,
 			os:          drivers.NodeStorage.NewSession(requestID),
 			sessManager: ls.AISessionManager,
+			orchAddr:    orchAddr,
 		}
 
 		if !async {
@@ -267,10 +271,12 @@ func (ls *LivepeerServer) LLM() http.Handler {
 
 		clog.V(common.VERBOSE).Infof(ctx, "Received LLM request prompt=%v model_id=%v stream=%v", req.Prompt, *req.ModelId, *req.Stream)
 
+		orchAddr := r.Header.Get("OrchAddr")
 		params := aiRequestParams{
 			node:        ls.LivepeerNode,
 			os:          drivers.NodeStorage.NewSession(requestID),
 			sessManager: ls.AISessionManager,
+			orchAddr:    orchAddr,
 		}
 
 		start := time.Now()
--- server/ai_process.go
+++ server/ai_process.go
@@ -74,6 +74,7 @@ type aiRequestParams struct {
 	node        *core.LivepeerNode
 	os          drivers.OSSession
 	sessManager *AISessionManager
+	orchAddr    string
 }
 
 // CalculateTextToImageLatencyScore computes the time taken per pixel for an text-to-image request.
@@ -1072,7 +1073,7 @@ func processAIRequest(ctx context.Context, params aiRequestParams, req interface
 		}
 
 		tries++
-		sess, err := params.sessManager.Select(ctx, cap, modelID)
+		sess, err := params.sessManager.Select(ctx, cap, modelID, params.orchAddr)
 		if err != nil {
 			clog.Infof(ctx, "Error selecting session modelID=%v err=%v", modelID, err)
 			continue
