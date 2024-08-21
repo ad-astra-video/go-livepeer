0365c22ab9234dc239666aadae0addd93c7b0a54
Brad P <brad@ad-astra.video>
2024-08-21 16:46:56 -0500

    enable selecting orchestrator to get AI job

--- server/ai_process.go
+++ server/ai_process.go
@@ -85,6 +85,7 @@ type aiRequestParams struct {
 	node        *core.LivepeerNode
 	os          drivers.OSSession
 	sessManager *AISessionManager
+	orchAddr    string
 
 	// For live video pipelines
 	segmentReader *media.SwitchableSegmentReader
@@ -1429,7 +1430,7 @@ func processAIRequest(ctx context.Context, params aiRequestParams, req interface
 		}
 
 		tries++
-		sess, err := params.sessManager.Select(ctx, cap, modelID)
+		sess, err := params.sessManager.Select(ctx, cap, modelID, params.orchAddr)
 		if err != nil {
 			clog.Infof(ctx, "Error selecting session modelID=%v err=%v", modelID, err)
 			continue
